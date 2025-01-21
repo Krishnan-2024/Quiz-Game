f5f95e479bf7b6f916582d89288d9f2c092496b0
Krishnan-2024 <sivakrishnas785@gmail.com>
2025-01-21 06:42:02 +0000

    /Quiz-Game-App

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -20,40 +20,40 @@ class Home extends Component {
   }
 
   renderLoader = () => (
-    <div data-testid='loader' className='loader'>
-      <Loader type='ThreeDots' color='#0b69ff' height={50} width={50} />
+    <div data-testid="loader" className="loader">
+      <Loader type="ThreeDots" color="#0b69ff" height={50} width={50} />
     </div>
   )
 
   renderHomePageContent = () => (
-    <div className='home-page-content'>
+    <div className="home-page-content">
       <img
-        src='https://assets.ccbp.in/frontend/react-js/quiz-game-start-the-quiz-img.png'
-        alt='start quiz game'
-        className='home-image'
+        src="https://assets.ccbp.in/frontend/react-js/quiz-game-start-the-quiz-img.png"
+        alt="start quiz game"
+        className="home-image"
       />
-      <h1 className='home-heading'>
+      <h1 className="home-heading">
         How Many Of These Questions Do You Actually Know?
       </h1>
-      <p className='home-description'>
+      <p className="home-description">
         Test yourself with these easy quiz questions and answers
       </p>
-      <Link to='/quiz-game'>
+      <Link to="/quiz-game">
         <button
-          className='start-button'
-          type='button'
+          className="start-button"
+          type="button"
           onClick={this.handleStartButtonClick}
         >
           Start Quiz
         </button>
       </Link>
-      <div className='warning-container'>
+      <div className="warning-container">
         <img
-          className='warning-image'
-          alt='warning icon'
-          src='https://assets.ccbp.in/frontend/react-js/quiz-game-error-img.png'
+          className="warning-image"
+          alt="warning icon"
+          src="https://assets.ccbp.in/frontend/react-js/quiz-game-error-img.png"
         />
-        <p className='warning-text'>
+        <p className="warning-text">
           All the progress will be lost, if you reload during the quiz
         </p>
       </div>
@@ -63,9 +63,9 @@ class Home extends Component {
   render() {
     const {isLoading} = this.state
     return (
-      <div className='home-container'>
+      <div className="home-container">
         <Header />
-        <div className='home-page'>
+        <div className="home-page">
           {isLoading ? this.renderLoader() : this.renderHomePageContent()}
         </div>
       </div>
